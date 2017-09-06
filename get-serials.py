4e9e7dbfad77419cbba9e35aa2471aa7b9ab76b0
Tom Lijnse <tom.lijnse@layerzero.nl>
2017-09-06 21:01:44 +0200

    Added get-serials.py

---
+++ get-serials.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""Obtain serial numbers of ACI fabric nodes using the Cobra SDK"""
+
+__author__ = 'Tom Lijnse'
+__version__ = '0.1'
+
+from cobra.mit.access import MoDirectory
+from cobra.mit.request import ClassQuery
+from cobra.mit.session import LoginSession
+
+from requests.packages.urllib3 import disable_warnings
+disable_warnings()
+
+apic_url = 'https://192.168.221.2'
+apic_user = 'admin'
+apic_password = '1234QWer'
+
+login_session = LoginSession(apic_url,apic_user,apic_password)
+md = MoDirectory(login_session)
+md.login()
+
+cq = ClassQuery('fabricNode')
+nodes = md.query(cq)
+
+print '\nNode name : Serial number\n'
+
+for node in nodes:
+	print '{0:9} : {1} '.format(node.name,node.serial)
